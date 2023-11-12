2d01a6979a34e69ecfc3f66eb21c7ce44da43331
Luka Skodnik <ls1906@student.uni-lj.si>
2023-11-12 16:37:49 +0100

    Construct Rossmann graph edges and connect the nodes

--- src/rosmann_to_graph.py
+++ src/rosmann_to_graph.py
@@ -23,6 +23,7 @@ FILE_ABS_PATH = pathlib.Path(__file__) # absolute path of this file
 # TODO: better error handling
 # TODO: fix so we dont map NaN's when encoding categories
 
+
 class CategoricalEncoder:
     def __init__(self):
         pass
@@ -37,6 +38,7 @@ class CategoricalEncoder:
             except Exception:
                 x[i, 0] = -1
         return x
+
 
 class OneHotEncoder:
     def __init__(self):
@@ -53,6 +55,7 @@ class OneHotEncoder:
                 x[i, :] = torch.from_numpy(np.zeros(len(categories)).astype(int))
         return x
 
+
 # TODO: maybe specify if we want floats or integers
 class NumericalEncoder:
     def __init__(self):
@@ -60,6 +63,7 @@ class NumericalEncoder:
     
     def __call__(self, series):
         return torch.tensor(series.values, dtype=torch.float).view(-1, 1)
+
 
 # NOTE: this acts like a one-hot encoder, but can have multiple 1s in a row
 # TODO: maybe write a more general class for this types of encoders?
@@ -78,6 +82,14 @@ class PromoIntervalEncoder:
         return x
 
 
+class IdentityEncoder:
+    def __init__(self, dtype=None):
+        self.dtype = dtype
+
+    def __call__(self, df):
+        return torch.from_numpy(df.values).view(-1, 1).to(self.dtype)
+
+
 ###########################################################################################
 ###########################################################################################
 
@@ -92,6 +104,25 @@ def load_node_csv(path, index_col, encoders=None, **kwargs):
         x = torch.cat(xs, dim=-1)
 
     return x, mapping
+
+
+def load_edge_csv(path, src_index_col, src_mapping, dst_index_col, dst_mapping, encoders=None, **kwargs):
+    df = pd.read_csv(path, **kwargs)
+    
+    # NOTE: since the "sales" df (train.csv) has no index column copy the default index to a column named "index"
+    df["index"] = df.index
+
+    src = [src_mapping[index] for index in df[src_index_col]]
+    dst = [dst_mapping[index] for index in df[dst_index_col]]
+    edge_index = torch.tensor([src, dst])
+
+    edge_attr = None
+    if encoders is not None:
+        edge_attrs = [encoder(df[col]) for col, encoder in encoders.items()]
+        edge_attr = torch.cat(edge_attrs, dim=-1)
+
+    return edge_index, edge_attr
+
 
 def rossmann_to_graph(dir_path):
     # paths of the csv files
@@ -127,12 +158,24 @@ def rossmann_to_graph(dir_path):
         "SchoolHoliday": CategoricalEncoder(),
     })
     
-    # TODO: create edges
+    # create edge index
+    edge_index, edge_label = load_edge_csv(
+        sales_train_path,
+        src_index_col='Store',
+        src_mapping=store_mapping,
+        dst_index_col="index",
+        dst_mapping=sales_mapping,
+        encoders=None # if we would want edge features
+    )
     
-    # put the nodes into a graph
+    # create empty graph
     data = HeteroData()
+    # put the nodes into a graph
     data["store"].x = store_x
     data["sales"].x = sales_x
+    # connect the nodes with edges
+    data["store", "sells", "sales"].edge_index = edge_index
+    data["store", "sells", "sales"].edge_label = edge_label
     
     return data
 
@@ -143,9 +186,7 @@ def rossmann_to_graph(dir_path):
 
 def main():
     dir_path = FILE_ABS_PATH.parent.parent / "data" / "rossmann"
-    temp = rossmann_to_graph(dir_path)
-    
-    pass
+    rossmann_to_graph(dir_path)
 
 if __name__ == "__main__":
     main()
