66c679910a176b506f32858ef218c1c243ddf2dc
Luka Skodnik <ls1906@student.uni-lj.si>
2023-12-13 17:32:56 +0100

    Refactor pytorch geometric dataset construction and sampling

--- src/data_modelling/pyg_datasets.py
+++ src/data_modelling/pyg_datasets.py
@@ -11,7 +11,20 @@ from src.data_modelling.table_to_graph import database_to_graph, database_to_sub
 from src.data_modelling.feature_engineering import add_index, add_k_hop_degrees, filter_graph_features_with_mapping
 
 ############################################################################################
-# reference: https://pytorch-geometric.readthedocs.io/en/latest/tutorial/create_dataset.html
+DEFAULTS = {
+    "rossmann-store-sales": {
+        "features": ["type"],
+        "feature_mappings": {"type": {"store": 0, "test": 1}},
+        "target": "k_hop_degrees"
+    },
+    "mutagenesis": {
+        "features": ["type"],
+        "feature_mappings": {"type": {"molecule": 0, "atom": 1, "bond": 2}},
+        "target": "k_hop_degrees"
+    },
+}
+
+
 ############################################################################################
 
 class GraphRelationalDataset(InMemoryDataset):
@@ -25,36 +38,35 @@ class GraphRelationalDataset(InMemoryDataset):
         return 'data.pt'
 
     def process(self):
-        if os.path.exists(self.root):
-            shutil.rmtree(self.root)
+        # TODO: make sure we overwrite the existing data if we call process
+        # if os.path.exists(self.root):
+        #     shutil.rmtree(self.root)
         self.save(self.data_list, self.processed_paths[0])
 
 
 ############################################################################################
 
 
-def sample_relational_distribution(dataset_name, num_graphs, seed=42):
-    if dataset_name == "rossmann-store-sales":
-        features = _ROSSMANN_FEATURES_TO_KEEP_GIN
-        features_length = _ROSSMANN_FEATURES_LENGTH
-        dataset_class = Rossmann
-        root = "data/pyg/rossmann_sample"
-        subgraphs = get_rossmann_subgraphs(features=features, feature_mappings=_ROSSMANN_FEATURE_MAPPING_GIN)
-    elif dataset_name == "mutagenesis":
-        features = _MUTAGENESIS_FEATURES_TO_KEEP_GIN
-        features_length = _MUTAGENESIS_FEATURES_LENGTH
-        dataset_class = Mutagenesis
-        root = "data/pyg/mutagenesis_sample"
-        subgraphs = get_mutagenesis_subgraphs(features=features, feature_mappings=_MUTAGENESIS_FEATURE_MAPPING_GIN)
-    else:
-        raise ValueError(f"Dataset {dataset_name} not supported")
+def sample_relational_distribution(dataset_name, num_graphs, features=None, feature_mappings=None, seed=42):    
+    # check if we need to set default parameters
+    if features is None:
+        features = DEFAULTS[dataset_name]["features"]
+    if feature_mappings is None:
+        feature_mappings = DEFAULTS[dataset_name]["feature_mappings"]
+    
+    
+    subgraphs, _ = database_to_subgraphs(dataset_name)
     
     # set seed and sample with replacement
     random.seed(seed)
     sampled_subgraphs = random.choices(subgraphs, k = num_graphs)
     
-    # avoid relabeling by using a list of graphs
+    # filter and map the features
+    sampled_subgraphs = [filter_graph_features_with_mapping(G, features, feature_mappings) for G in sampled_subgraphs]
+
+    # convert to pytorch geometric Data objects
     sampled_subgraphs = [from_networkx(G, group_node_attrs=features) for G in sampled_subgraphs]
+    
     previous_graph_nodes = 0
     for i in range(len(sampled_subgraphs)):
         sampled_subgraphs[i].edge_index += previous_graph_nodes
@@ -65,18 +77,24 @@ def sample_relational_distribution(dataset_name, num_graphs, seed=42):
         all_graphs_data.edge_index = torch.cat((all_graphs_data.edge_index, data.edge_index), dim=1)
         all_graphs_data.x = torch.cat((all_graphs_data.x, data.x), dim=0)
     all_graphs_data.index = torch.tensor(range(all_graphs_data.x.shape[0]))
-    all_graphs_data.x = all_graphs_data.x[:, 1:(features_length + 1)].type(torch.float)
-    data_list = [all_graphs_data]    
     
-    if os.path.exists(root):
-        shutil.rmtree(root)
-    
-    dataset = dataset_class(root=root, data_list=data_list)
+    dataset = GraphRelationalDataset(root=f"data/pyg/{dataset_name}/sample", data_list=[all_graphs_data])
     dataset.process()
     return dataset
 
 
-def create_pyg_dataset(dataset_name, pyg_dataset_save_path, features, feature_mappings, target):
+############################################################################################
+
+
+def create_pyg_dataset(dataset_name, features=None, feature_mappings=None, target=None):
+    # check if we need to set default parameters
+    if features is None:
+        features = DEFAULTS[dataset_name]["features"]
+    if feature_mappings is None:
+        feature_mappings = DEFAULTS[dataset_name]["feature_mappings"]
+    if target is None:
+        target = DEFAULTS[dataset_name]["target"]
+    
     
     # load the graph representing the database
     G, _ = database_to_graph(dataset_name)
@@ -101,22 +119,20 @@ def create_pyg_dataset(dataset_name, pyg_dataset_save_path, features, feature_ma
     data.x = data.x[:, 1:(ncols - target_length)].type(torch.float)
     
     
-    dataset = GraphRelationalDataset(root=pyg_dataset_save_path, data_list=[data])
+    dataset = GraphRelationalDataset(root=f"data/pyg/{dataset_name}", data_list=[data])
     dataset.process()
     return dataset
 
 
 ############################################################################################
 
-def main():
-    rossmann_feature_mappings = {"type": {"store": 0, "sale": 1}}
-    rossmann_dataset = create_pyg_dataset("rossmann-store-sales", "data/pyg/rossmann", ["type"], rossmann_feature_mappings, "k_hop_degrees")
+def main():    
+    rossmann_dataset = create_pyg_dataset("rossmann-store-sales")
+    mutagenesis_dataset = create_pyg_dataset("mutagenesis")
     
-    mutagenesis_feature_mappings = {"type": {"molecule": 0, "atom": 1, "bond": 2}}
-    mutagenesis_dataset = create_pyg_dataset("mutagenesis", "data/pyg/mutagenesis", ["type"], mutagenesis_feature_mappings, "k_hop_degrees")
+    rossmann_sample = sample_relational_distribution("rossmann-store-sales", 1000, seed=420)
+    mutagenesis_sample = sample_relational_distribution("mutagenesis",  1000, seed=420)
     
-    # rossmann_sample = sample_relational_distribution("rossmann-store-sales", 1000, seed=420)
-    # mutagenesis_sample = sample_relational_distribution("mutagenesis", 10, seed=420)
     pass
 
 if __name__ == "__main__":
