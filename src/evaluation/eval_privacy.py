c07b1098534a2ee292359dfe170feb2d5674a0b5
uroskozole <kozole.uros@gmail.com>
2023-12-17 00:28:02 +0100

    finished functionality of privacy eval

--- src/evaluation/eval_privacy.py
+++ src/evaluation/eval_privacy.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.model_selection import train_test_split
+import argparse
 
+from src.data.utils import merge_children, load_tables, load_metadata, get_root_table
+from eval_classifier import CustomHyperTransformer, drop_ids
 
 # PRIVACY
 def distance_to_closest_record(original, synthetic, **kwargs):
@@ -32,33 +35,87 @@ def nearest_neighbor_distribution_score(original, synthetic, **kwargs):
     D_1, D_2 = train_test_split(original, test_size=0.5)
 
     # calculate the 5th/x-th percentile 
-    ratios = calculate_ratios(D_1, D_2, **kwargs)
+    ratios = calculate_ratios(D_1, D_2)
     alpha = np.percentile(ratios, 5)
 
     # calculate the ratios with synthetic data
-    ratios_syn = calculate_ratios(original, synthetic, **kwargs)
+    ratios_syn = calculate_ratios(original, synthetic)
 
     privacy = np.mean(ratios_syn < alpha)
 
     return privacy
 
 
-def calculate_ratios(D_1, D_2, **kwargs):
-    if kwargs["privacy_percetile"]:
-        privacy_percentile = kwargs["privacy_percetile"]
-    else:
-        privacy_percentile = 0.05
-    
+def calculate_ratios(D_1, D_2):
     # calculate nearest neighbor distances for D_1 with itself and D_1 with D_2
-    self_distance = pairwise_distances(D_1, matric="manhattan", **kwargs)
-    self_distance = np.min(self_distance, axis=1)
-    mutual_distance = pairwise_distances(D_1, D_2, matric="manhattan", **kwargs)
+    self_distance = pairwise_distances(D_1, metric="manhattan")
+    self_distance = np.partition(self_distance, 1, axis=1)[:, 1] # np.min is distance to itself so we need to take second min
+    mutual_distance = pairwise_distances(D_1, D_2, metric="manhattan")
     mutual_distance = np.min(mutual_distance, axis=1)
 
     ratios = mutual_distance / self_distance
     
     return ratios
+
+
+def get_args():
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='LP and lep pozdrav')
+
+    # dataset to evaluate
+    parser.add_argument('--dataset', type=str, default='rossmann-store-sales',
+                        help='Specify the dataset to evaluate.')
+    
+    parser.add_argument('--method', type=str, default='sdv', 
+                        help='Specify the synthetic data generation method to evaluate')
+    
+    parser.add_argument('--privacy-percetile', type=float, default=None,
+                        help='Parameter for privacy calculation')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    return args
      
 if __name__ == "__main__":
-    # df1 = pd
-    pass
+
+    args = get_args()    
+    
+    tables_synthetic = load_tables(args.dataset, data_type=f'synthetic/{args.method}')
+    tables_original = load_tables(args.dataset, split='train')
+
+    metadata = load_metadata(args.dataset)
+    root_table = get_root_table(args.dataset)
+
+    original = merge_children(tables_original, metadata, root_table)
+    synthetic = merge_children(tables_synthetic, metadata, root_table)
+
+    # drop all foreign and primary keys
+    for table in metadata.to_dict()['tables'].keys():
+        drop_ids(original, table, metadata)
+        drop_ids(synthetic, table, metadata)
+
+    transformed_original = original.copy()
+    transformed_synthetic = synthetic.copy()
+
+    column_names = transformed_original.columns.to_list()
+    transformed_original = transformed_original.reindex(column_names, axis=1)
+    transformed_synthetic = transformed_synthetic.reindex(column_names, axis=1)
+
+    max_items = 100000
+
+    n = min(max_items, transformed_original.shape[0], transformed_synthetic.shape[0])
+    transformed_original = transformed_original.sample(n=n, random_state=42, replace=False)
+    transformed_synthetic = transformed_synthetic.sample(n=n, random_state=42, replace=False)
+
+    if 'Date' in column_names:
+        transformed_original['Date'] = pd.to_numeric(pd.to_datetime(transformed_original['Date']))
+        transformed_synthetic['Date'] = pd.to_numeric(pd.to_datetime(transformed_synthetic['Date']))
+
+    ht = CustomHyperTransformer()
+    transformed_original = ht.fit_transform(transformed_original)
+    transformed_synthetic = ht.fit_transform(transformed_synthetic)
+    
+    privacy_score = nearest_neighbor_distribution_score(transformed_original, transformed_synthetic, privacy_percetile=args.privacy_percetile)
+
+    print(privacy_score)
